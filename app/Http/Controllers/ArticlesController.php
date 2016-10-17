b9bc3088b4885ef1276710c30fca20396103c27c
Penthious <tleffew1994@gmail.com>
2016-10-17 17:07:57 -0600

    fixed articles layout

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -15,7 +15,6 @@ class ArticlesController extends Controller
 
     public function index()
     {
-        return 'test';
-        return view('');
+        return view('sections.articles.index');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <!-- Styles -->
         <style>
             html, body {
@@ -52,6 +52,17 @@
         <div class="container">
             @yield('content')
         </div>
+        <div class="container">
+            <div class="footer">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                    Contact
+                </button>
+            </div>
+        </div>
 
+        @include('sections.contact.contact_modal')
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
