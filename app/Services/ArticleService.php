0b25e95e4419b23e69f07223db4d0ddaa7884b69
Penthious <tleffew1994@gmail.com>
2016-10-27 22:28:03 -0600

    added controller layouts for articles and keywords

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use DB;
 use App;
 use App\Repositories\ArticleRepository;
 
@@ -12,10 +13,35 @@ class ArticleService
     /**
      * @var ArticleRepository
      */
+    protected $articleRepository;
 
-    protected $ArticleRepository;
-
+    /**
+     * ArticleService constructor.
+     * @param ArticleRepository $articleRepository
+     */
     public function __construct( ArticleRepository $articleRepository )
     {
+        $this->articleRepository = $articleRepository;
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function create( Array $attributes )
+    {
+        DB::transaction(function () use ( $attributes ) {
+            $this->articleRepository->create($attributes);
+        });
+    }
+
+    /**
+     * @param array $attributes
+     * @param $id
+     */
+    public function update( Array $attributes, $id )
+    {
+        DB::transaction(function () use ( $attributes, $id ) {
+            $this->articleRepository->update($attributes, $id);
+        });
     }
 }
--- app/Services/KeywordService.php
+++ app/Services/KeywordService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App;
 use App\Repositories\KeywordRepository;
+use DB;
 
 
 class KeywordService
@@ -15,8 +16,33 @@ class KeywordService
 
     protected $keywordRepository;
 
+    /**
+     * KeywordService constructor.
+     * @param KeywordRepository $keywordRepository
+     */
     public function __construct( KeywordRepository $keywordRepository )
     {
         $this->keywordRepository = $keywordRepository;
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function create( Array $attributes)
+    {
+        DB::transaction(function () use ($attributes){
+            $this->keywordRepository->create($attributes);
+        });
+    }
+
+    /**
+     * @param array $attributes
+     * @param $id
+     */
+    public function update( Array $attributes, $id)
+    {
+        DB::transaction(function () use ($attributes, $id){
+            $this->keywordRepository->create($attributes, $id);
+        });
     }
 }
