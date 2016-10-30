b8fefdbc615c2512a4b216244ef513bc084ed70a
Penthious <tleffew1994@gmail.com>
2016-10-29 18:43:51 -0600

    added laravel collection and datatbles

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Yajra\Datatables\DatatablesServiceProvider::class,
         //
 
         /*
@@ -226,6 +228,17 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /**
+         * laravel collective facade
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
+        /**
+         * DataTables facade
+         */
+        'DataTables' => Yajra\Datatables\Facades\Datatables::class,
 
     ],
 
