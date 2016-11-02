e276f44f6aa9a1c2c1c16d5940bf0b2ace1a3e8e
CJ Sampson <cjsampson41@gmail.com>
2016-11-02 14:01:34 -0600

    static elements displayed for the containers

--- resources/views/sections/videos/index.blade.php
+++ resources/views/sections/videos/index.blade.php
@@ -12,31 +12,58 @@
         <h2 class="video--title">Video Section</h2>
 
         <div class="video--box--container">
-            <div class="video--box">span </div>                        
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 2</div>                    
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 3</div>            
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 4</div>            
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 5</div> 
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 6</div>
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 7</div>
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 7</div>
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>
         <div class="video--box--container">
-            <div class="video--box">Video 8</div>
+            <div class="video--box">
+                <div class="video--box--iframe"></div>
+                <p class="video--box--description">Description</p>
+            </div>
         </div>        
     </div>
 
