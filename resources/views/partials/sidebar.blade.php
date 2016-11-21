893cede3014f8ec97e36f0eec9ab37ef68d02e3c
CJ Sampson <cjsampson41@gmail.com>
2016-11-20 17:51:56 -0700

    sidebar with icons

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,4 +2,49 @@
     <button class="closeSidebar">
         close me
     </button>
+
+    <div class="sidebar--navigation">
+
+    </div>
+
+    <div class="sidebar--options">
+        <ul>
+
+            <li>
+                <a href="#">
+                    <img src="/images/javascript-sidebar.png" alt="JavaScript Sidebar Icon"  class="sidebar--option--image">
+                    <span class="sidebar--option--text">JavaScript</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/images/jquery-sidebar.gif" alt="jQuery Sidebar Icon"  class="sidebar--option--image">
+                    <span class="sidebar--option--text">jQuery</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/images/react-sidebar.png" alt="React Sidebar Icon"  class="sidebar--option--image">
+                    <span class="sidebar--option--text">React</span>
+                </a>
+            </li>
+
+            <li>            
+                <a href="#">
+                    <img src="/images/php-sidebar.png" alt="PHP Sidebar Icon"  class="sidebar--option--image">
+                    <span class="sidebar--option--text">PHP</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/images/laravel-sidebar.png" alt="Laravel Sidebar Icon"  class="sidebar--option--image">
+                    <span class="sidebar--option--text">Laravel</span>
+                </a>
+            </li>
+
+        </ul>        
+    </div>
 </div>
