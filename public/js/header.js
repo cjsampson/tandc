ebce8a0145e7d8bb6912712af2c892081f4c05d6
CJ Sampson <cjsampson41@gmail.com>
2016-11-21 10:00:09 -0700

    shortened the scroll for the header

--- public/js/header.js
+++ public/js/header.js
@@ -1,7 +1,7 @@
 function init() {
   window.addEventListener('scroll', function(e){
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-        shrinkOn = 200,
+        shrinkOn = 35,
         header = document.querySelector("header");
     if (distanceY > shrinkOn) {
       header.setAttribute("class","smaller");
