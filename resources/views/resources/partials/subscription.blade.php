38bb832fd4a29cc9fac0f462ae6f01ce68103429
CJ Sampson <cjsampson41@gmail.com>
2016-11-29 15:38:26 -0700

    all image paths should be correct

--- resources/views/resources/partials/subscription.blade.php
+++ resources/views/resources/partials/subscription.blade.php
@@ -4,7 +4,7 @@
 <p>https://www.laracasts.com</p>
 
 <div class="resources--image">
-    <img src="/images/laracasts.png" alt="Laracasts">
+    <img src="/images/subscription/laracasts.png" alt="Laracasts">
 </div>
 
 <p>Laracasts is the ultimate online learning source for all things PHP and more.  Jeffrey Way definitely has an elite mastery of web development and teaching combined.  Laracasts starts out for only $9 a month and the site has loads and loads of content ranging from beginner PHP and JavaScript, to more advanced videos on Frontend and Backend frameworks/libraries.  You're money for spent on this site is a good investment.</p>
@@ -14,7 +14,7 @@
 <h3>Treehouse</h3>
 <p>https://www.teamtreehouse.com</p>
 
-<img src="/images/teamtreehouse.png" alt="Team Tree House">
+<img src="/images/subscription/teamtreehouse.png" alt="Team Tree House">
 <p>For only $25 a month, Treehouse gives you loads of different learning tracks for beginners starting out in web development.  Treehouse has loads of features from Forums, Screencast, Interactive Coding environments, and much more.  I really like Treehouse because they have content for web-designers, front-end, and backend as well!</p>
 
 <br>
@@ -22,7 +22,7 @@
 <h3>Codeschool</h3>
 <p>https://www.codeschool.com</p>
 
-<img src="/images/codeschool.png" alt="Code School">
+<img src="/images/subscription/codeschool.png" alt="Code School">
 
 <p>Codeschool is one of my favorite online learning resources being that it was one of my first paid online subscriptions that took my coding skills to the next level.  They have a bunch of interactive coding environments and tracks for users in JavaScript, Ruby, CSS, Git, and loads more.  If you're looking for a fun way to spice up your programming learning, I highly recommend Codeschool!</p>
 
@@ -31,7 +31,7 @@
 <h3>Codecademy</h3>
 <p>https://www.codecademy.com</p>
 
-<img src="/images/codecademy.png" alt="Code Cademy">
+<img src="/images/subscription/codecademy.png" alt="Code Cademy">
 
 <p>Codecademy has loads of free content and also offers premium content for $19.99 a month.  Codecademy is one of the industry leaders in helping beginners get acclamated with coding.  From a range of tracks for learning front-end, backend, APIs, and also building full projects!  Make a visit to Codecademy to increase your skills for programming to get a new job!</p>
 
@@ -40,5 +40,5 @@
 <h3>Frontend Masters</h3>
 <p>https://www.frontendmasters.com</p>
 
-<img src="/images/frontendmasters.png" alt="Front End Masters">
+<img src="/images/subscription/frontendmasters.png" alt="Front End Masters">
 <p>Frontend Masters is an online learning application that has video series in the latest web development technologies and stacks with industry leading teachers that break down concepts for beginners and seasoned vets.  Brian Holt from Netflix, Ryan Florence the React.js master, and Kyle Simpson the ultimate JavaScript guru!  Frontend masters may seem more on the pricey side at $40 a month, but the in depth tutorials and video series is well worth the investment.  Head to Frontend Masters now for an awesome educational experience!</p>
