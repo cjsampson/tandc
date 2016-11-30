7e0d9c8aa25b54b600cd92f49c185e8d24cacec5
CJ Sampson <cjsampson41@gmail.com>
2016-11-29 22:50:59 -0700

    resources updated. coding challenges needs content

--- resources/views/resources/partials/youtube.blade.php
+++ resources/views/resources/partials/youtube.blade.php
@@ -1,59 +1,113 @@
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1">
+            <p>
+                <a href="https://www.youtube.com/channel/UCVTlvUkGslCV_h-nSAId8Sw">LearnCode.academy</a> is one of favorite YouTube pitstops for great tutorials on JavaScript.  From tutorials ranging from vanilla JavaScript, jQuery, Node, React, and much more, you're bound to find some bits of information to add to your JavaScript knowledge base.
+            </p>
+        </div>
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/learncodeacademy.png" alt="Learn Code Cademy">
+            </figure>
+        </div>
+    </div>
+</div>
 
-<h1>YouTube Channels</h1>
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/codecourse.png" alt="Code Course">
+            </figure>
+        </div>
 
-<br>
-
-<h3>LearnCode.academy</h3>
-<p>https://www.youtube.com/channel/UCVTlvUkGslCV_h-nSAId8Sw</p>
-
-<img src="/images/youtube/learncodeacademy.png" alt="Learn Code Cademy">
-<p>LearnCode.academy is one of favorite YouTube pitstops for great tutorials on JavaScript.  From tutorials ranging from vanilla JavaScript, jQuery, Node, React, and much more, you're bound to find some bits of information to add to your JavaScript knowledge base.</p>
-
-<br>
-
-<h1>Codecourse</h1>
-<p><a href="https://www.youtube.com/channel/UCpOIUW62tnJTtpWFABxWZ8g">CodeCourse</a></p>
-
-<img src="/images/youtube/codecourse.png" alt="Code Course">
-<p>Codecourse is one of my top ranked YouTube channels because of the depth of knowledge and the numerous amounts of videos ranging from the front and back end of the web.  Codecourse's channel has many video series building full web apps that can really help your understanding of the complete web development process.  Codecourse has many PHP/Laravel tutorials as well if you're a backend developer looking to learn how to build a site with a framework. Codecourse also has a website with premium content for only $5 dollars.</p>
-
-<br>
-
-<h3>funfunfunction</h3>
-<p>https://www.youtube.com/channel/UCO1cgjhGzsSYb1rsB4bFe4Q</p>
-
-<img src="/images/youtube/funfunfunction.png" alt="Fun Fun Function">
-
-<p>If you're a JavaScript developer and need a deeper insight on JS constructs, funfunfunction's YouTube channel is the place for you.  This is one of my top YouTube learning channels because there are very in depth looks at different types of JavaScript patterns and native functions that help you break down the meaning and usage in particular situations.  After your have mastered the basics and syntax of JavaScript, I highliy recommend you taking a few days to run through this channel's videos to take your skills a step further.</p>
-
-
-<h3>Simple Programmer</h3>
-<p>https://www.youtube.com/channel/UCFxdcuY-S6yjZGq_2cjilHg</p>
-
-<img src="/images/youtube/simpleprogrammer.png" alt="Simple Programmer">
-<p>John Sonmez of Simple Programmer has loads of questions and videos for programmers and individuals getting started in the industry.  He has a lot of videos answering fans directly on certain questions and gives his advice on how to start or transition their career into programming or web developement.</p>
-
-<br>
-
-<h3>Eli the Computer Guy</h3>
-<p>https://www.youtube.com/user/elithecomputerguy</p>
-
-<img src="/images/youtube/elithecomputerguy.png" alt="Eli The Computer Guy">
-
-<p>Eli the Computer Guy is an experienced industry professional who answers loads of questions on the IT/Computer Industry.  This channel breaks down topics from Database Administration, Networking, Backend programming, and much much more.  I really love this channel because it gives you valuable insight from a seasoned professional with loads of experience.</p>
-
-<br>
-<h3>The New Boston</h3>
-<p>https://www.youtube.com/user/thenewboston</p>
-
-<img src="/images/youtube/thenewboston.png" alt="The New Boston">
-<p>The New Boston YouTube channel is very dear to my heart being that it was one of the first online learning sources I grabbed ahold too early on in my career.  Bucky has a HUGE range of tutorials on about every programming language known to man.  If you're new to the programming scene or are about to start to learn a new language,  The New Boston is your best bet. </p>
+        <div class="column is-4 is-offset-1">
+            <p>
+                <a href="https://www.youtube.com/channel/UCpOIUW62tnJTtpWFABxWZ8g">CodeCourse</a> is one of my top ranked YouTube channels because of the depth of knowledge and the numerous amounts of videos ranging from the front and back end of the web.  Codecourse's channel has many video series building full web apps that can really help your understanding of the complete web development process.  Codecourse has many PHP/Laravel tutorials as well if you're a backend developer looking to learn how to build a site with a framework. Codecourse also has a website with premium content for only $5 dollars.
+            </p>
+        </div>
+    </div>
+</div>
 
 
-<br>
-<h3>Mackenzie Child</h3>
-<p>https://www.youtube.com/channel/UCfWZwsP8trUy5uHJg8gcGIQ</p>
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1">
+            <p>
+                If you're a JavaScript developer and need a deeper insight on JS constructs, <a href="https://www.youtube.com/channel/UCO1cgjhGzsSYb1rsB4bFe4Q">funfunfunction's</a> YouTube channel is the place for you.  This is one of my top YouTube learning channels because there are very in depth looks at different types of JavaScript patterns and native functions that help you break down the meaning and usage in particular situations.  After your have mastered the basics and syntax of JavaScript, I highliy recommend you taking a few days to run through this channel's videos to take your skills a step further.
+            </p>
+        </div>
 
-<img src="/images/youtube/mackenziechild.png" alt="Mackenzie Child">
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/funfunfunction.png" alt="Fun Fun Function">
+            </figure>
+        </div>
+    </div>
+</div>
 
-<p>Although I am not a web designer, Mackenzie Child's YouTube channel is a great place for trying to learn front-end/web-designer terminology.  Mackenzie makes some pretty sweet looking sites and has a bunch of different tutorials and video series breaking things down on the front-end.  If your a developer with some weak front-end/designer chops, this channel is great for you!</p>
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/simpleprogrammer.png" alt="Simple Programmer">
+            </figure>
+        </div>
+
+        <div class="column is-4 is-offset-1">
+            <p>
+                <a href="https://www.youtube.com/channel/UCFxdcuY-S6yjZGq_2cjilHg">John Sonmez</a> of Simple Programmer has loads of questions and videos for programmers and individuals getting started in the industry.  He has a lot of videos answering fans directly on certain questions and gives his advice on how to start or transition their career into programming or web developement.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1">
+            <p>
+                <a href="https://www.youtube.com/user/elithecomputerguy">Eli the Computer Guy</a> is an experienced industry professional who answers loads of questions on the IT/Computer Industry.  This channel breaks down topics from Database Administration, Networking, Backend programming, and much much more.  I really love this channel because it gives you valuable insight from a seasoned professional with loads of experience.</p>
+            </p>
+        </div>
+
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/elithecomputerguy.png" alt="Eli The Computer Guy">
+            </figure>
+        </div>
+    </div>
+</div>
+
+<div class="columns subscription-box container content">
+    <div class="columns">
+
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/thenewboston.png" alt="The New Boston">
+            </figure>
+        </div>
+
+        <div class="column is-4 is-offset-1">
+            <p>
+                <a href="https://www.youtube.com/user/thenewboston">The New Boston</a> YouTube channel is very dear to my heart being that it was one of the first online learning sources I grabbed ahold too early on in my career.  Bucky has a HUGE range of tutorials on about every programming language known to man.  If you're new to the programming scene or are about to start to learn a new language,  The New Boston is your best bet. 
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="columns subscription-box container content">
+    <div class="columns">
+        <div class="column is-4 is-offset-1">
+            <p>
+                Although I am not a web designer, <a href="https://www.youtube.com/channel/UCfWZwsP8trUy5uHJg8gcGIQ">Mackenzie Child's</a> YouTube channel is a great place for trying to learn front-end/web-designer terminology.  Mackenzie makes some pretty sweet looking sites and has a bunch of different tutorials and video series breaking things down on the front-end.  If your a developer with some weak front-end/designer chops, this channel is great for you!
+            </p>
+        </div>
+        
+        <div class="column is-4 is-offset-1 card-column">
+            <figure class="image is-4by1 image-figure">
+                <img src="/images/youtube/mackenziechild.png" alt="Mackenzie Child">
+            </figure>
+        </div>
+
+    </div>
+</div>
